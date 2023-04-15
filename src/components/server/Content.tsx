46cb27323e458767677db8a14f91cebd673a24bc
nishiura-hiroki <nishiurahiroki@gmail.com>
2023-04-15 15:59:25 +0900

    Add categories display.

--- src/components/server/Content.tsx
+++ src/components/server/Content.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Blog } from '../../models/Blog';
 
 export type Props = {
@@ -10,12 +11,28 @@ export async function getContent(contentId: string) {
   return result;
 }
 
+export async function getCategories(contentId: string) {
+  const response = await fetch(`${process.env.API_HOST}/blog?id=${contentId}`);
+  const result: Blog = await response.json();
+  return result.category?.map(({ id, name }) => ({ id, name }));
+}
+
 export async function Content({ id }: Props) {
   const content = await getContent(id);
+  const categories = await getCategories(id);
 
   return (
     <div>
       <h1>{content.title}</h1>
+      <div>
+        <ul>
+          {categories?.map((category) => (
+            <Link href="/" key={category.id}>
+              <li>{category.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
       <div
         dangerouslySetInnerHTML={{
           __html: `${content.content}`,

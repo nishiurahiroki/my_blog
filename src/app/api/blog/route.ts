da1f0eaf695e2088387538cce59828f145bf91cf
nishiura-hiroki <nishiurahiroki@gmail.com>
2023-04-16 14:33:37 +0900

    Add blog post api.

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,4 +1,7 @@
-import { NextResponse } from 'next/server';
+import { title } from 'process';
+import { title } from 'process';
+import { NextRequest, NextResponse } from 'next/server';
+import { headers } from 'next/headers';
 import { client } from '../../../libs/client';
 import { Blog } from '../../../models/Blog';
 
@@ -18,4 +21,37 @@ export async function GET(request: Request) {
   }
   const data = await getContent(id);
   return NextResponse.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  const headersList = headers();
+
+  if (!headersList.get('authorization')) {
+    return NextResponse.json({
+      error: 'Token none.',
+    });
+  }
+
+  const apiKey = headersList.get('authorization')?.split(' ')[1];
+  if (apiKey !== process.env.SECRET_TOKEN) {
+    return NextResponse.json({
+      error: 'Token invalid.',
+    });
+  }
+
+  const body = await request.json();
+  const title = body.title;
+  const content = body.content;
+
+  const result = await client.create({
+    endpoint: 'blogs',
+    content: {
+      title,
+      content,
+    },
+  });
+
+  return NextResponse.json({
+    result,
+  });
 }

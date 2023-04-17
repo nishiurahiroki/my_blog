4527958977e5a875cac5d07f460fd74286c4833b
nishiura-hiroki <nishiurahiroki@gmail.com>
2023-04-17 23:49:02 +0900

    Fix top page css.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { client } from '../libs/client';
 import { Blogs } from '../models/Blog';
 
+import styles from './styles.module.css';
+
 async function getBlogTitles() {
   const result = await client.get<Blogs>({
     endpoint: 'blogs',
@@ -16,14 +18,14 @@ export default async function Page() {
   const titles = await getBlogTitles();
 
   return (
-    <div>
+    <div className={styles.container}>
       <header>
         <h2>AIが書いた怪談</h2>
       </header>
 
-      <div>
+      <div className={styles.articles}>
         {titles.map((title) => (
-          <div key={title.id}>
+          <div key={title.id} className={styles.article}>
             <Link href={`/content/${title.id}`}>{title.title}</Link>
           </div>
         ))}
